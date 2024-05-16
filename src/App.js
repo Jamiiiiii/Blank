2187e6caa3ef1fe3b55950ea158307be889c6844
Jamiiiiii <jami34@windowslive.com>
2024-05-16 05:38:13 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import LandingPage from './Components/LandingPage/LandingPage';
 import SignUp from './Components/SignUp/SignUp';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation'; 
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+
 
 function App() {
 
@@ -20,6 +22,7 @@ function App() {
               <Route path="/SignUp" element={<SignUp/>}/>
               <Route path="/Login" element={<Login/>}/>
               <Route path="/instantConsultation" element={<InstantConsultation />} />
+              <Route path="/FindDoctorSearch" element={<FindDoctorSearch/>}/>
               </Routes>
             
         </BrowserRouter>

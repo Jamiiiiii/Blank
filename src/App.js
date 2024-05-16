99f7e55f1797dacc75ebab012710203bf36054b4
Jamiiiiii <jami34@windowslive.com>
2024-05-16 06:51:08 -0400

    juu

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Login from './Components/Login/Login';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation'; 
 import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 import Notification from './Components/Notification/Notification';
+import Notification from './Components/ReviewForm/ReviewForm';
 
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
               <Route path="/SignUp" element={<SignUp/>}/>
               <Route path="/Login" element={<Login/>}/>
               <Route path="/instantConsultation" element={<InstantConsultation />} />
+              <Route path="/ReviewForm" element={<ReviewForm />} />
               <Route path="/FindDoctorSearch" element={<FindDoctorSearch/>}/>
               </Routes>
 
